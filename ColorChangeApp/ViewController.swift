61293ba170b7ee5a0d912059260b5f7069865256
meanmonkey662 <coolkoch8535@gmail.com>
2015-12-15 13:25:02 -0700

    Initial Commit

---
+++ ColorChangeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorChangeApp
+//
+//  Created by Koch, Kyle on 12/15/15.
+//  Copyright © 2015 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
